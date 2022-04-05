92c07a6e3b65b734a9157c1ac157d9c3940988fe
Melbin Mathew <melbin1997@gmail.com>
2022-04-06 00:15:52 +0530

    Github Actions Test

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,44 +1,23 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import RequestFactory, TestCase
 
-from tasks.models import Task
+from .views import GenericTaskView
 
-class TestTaskManager(TestCase):
-    def test_add_task(self):
-        """Test Adding Tasks"""
-        task_name = "Test1"
-        response = self.client.get("/add-task/", {"task": task_name})
-        self.assertEqual(response.status_code, 302)
+class QuestionModelTests(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username="bruce_wayne", email="bruce@wayne.org", password="i_am_batman")
 
-    def test_delete_task(self):
-        """Test Deleting Tasks"""
-        task_name = "Test2"
-        self.client.get("/add-task/", {"task": task_name})
-        task_object = Task.objects.filter(title=task_name).first()
-        self.assertIsNotNone(task_object)
-        response = self.client.get(f"/delete-task/{task_object.id}/")
-        self.assertEqual(response.status_code, 302)
-
-    def test_a_view_task(self):
-        """Test Viewing Tasks"""
-        task_name = "learn to use the force"
-        self.client.get("/add-task/", {"task": task_name})
-        response = self.client.get("/tasks/")
+    def test_authenticated(self):
+        """
+        Try to GET the tasks listing page, expect the response to redirect to the login page
+        """
+        # Create an instance of a GET request.
+        request = self.factory.get("/tasks/")
+        # Set the user instance on the request.
+        request.user = self.user
+        # We simply create the view and call it like a regular function
+        response = GenericTaskView.as_view()(request)
+        # Since we are authenticated we get a 200 response
         self.assertEqual(response.status_code, 200)
-        self.assertIn(task_name, response.content.decode())
-
-    def test_b_view_task(self):
-        """Complete a given task and ensure its in the completed view"""
-        task_name = "learn the ways of the force"
-        self.client.get("/add-task/", {"task": task_name})
-        response = self.client.get("/completed_tasks/")
-        self.assertEqual(response.status_code, 200)
-        self.assertNotIn(task_name, response.content.decode())
-        task_object = Task.objects.filter(title=task_name).first()
-        self.assertIsNotNone(task_object)
-        t = self.client.get(f"/complete_task/{task_object.id}/")
-        response = self.client.get("/tasks/")
-        self.assertEqual(response.status_code, 200)
-        self.assertNotIn(task_name, response.content.decode())
-        response = self.client.get("/completed_tasks/")
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(task_name, response.content.decode())
