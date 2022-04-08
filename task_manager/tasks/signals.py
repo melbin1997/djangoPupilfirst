bf90fc7ea98971dd1dc6496da15e372d39bffb51
Melbin Mathew <melbin1997@gmail.com>
2022-04-08 19:17:25 +0530

    Fixed issue in signals

--- task_manager/tasks/signals.py
+++ task_manager/tasks/signals.py
@@ -5,6 +5,8 @@ from .models import Task, TaskStatusChange
 @receiver(pre_save, sender=Task)
 def task_status_change(sender, instance, *args, **kwargs):
     # print(f'sender : {sender} \n instance : {instance} \n args : {args} \n kwargs : {kwargs} ')
-    oldStatus = Task.objects.filter(id=instance.id).first().status
-    if oldStatus != instance.status:
-        TaskStatusChange(old_status=oldStatus, new_status=instance.status, task=instance).save()
+    task = Task.objects.filter(id=instance.id).first()
+    if task != None:
+        oldStatus = task.status
+        if oldStatus != instance.status:
+            TaskStatusChange(old_status=oldStatus, new_status=instance.status, task=instance).save()
